4db526c14f9295af4186ed4ef4fcf090ec3fb481
Joel Buenrostro <joelbuenrostro@outlook.com>
2024-02-21 23:57:00 -0600

    BN-38 Pagina de inicio de sesion
    Creacion de componente y pantalla de inicio de sesion

---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import FormContainer from '../components/FormContainer'
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log('submit')
+  }
+
+  return (
+    <FormContainer>
+      <h1>Inicia sesion</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Ingresa tu email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='password'>
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Ingresa tu contraseña'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='primary'>
+          Iniciar sesion
+        </Button>
+      </Form>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
